8e2aab23bc693b68cd937b8df241ab8e174d4de7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-05 13:13:21 +0100

    Help for CheckConfiguration dialog

--- src/gui/dialogs/check_configuration.py
+++ src/gui/dialogs/check_configuration.py
@@ -2,7 +2,7 @@
 
 import tkinter
 
-from gui.dialogs.help_dialog import show_help
+from gui.dialogs.help_dialog import show_help_for_configuration_dialog
 from gui.icons import Icons
 
 
@@ -54,4 +54,4 @@ class CheckConfigurationDialog(tkinter.Toplevel):
 
     def help(self) -> None:
         """Handle Help button press."""
-        show_help()
+        show_help_for_configuration_dialog()
