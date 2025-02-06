0e9b87537e62b97462984cc10feb0cbdae0701a6
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-06 20:40:10 +0100

    Help for default model dialog

--- src/gui/dialogs/default_model_dialog.py
+++ src/gui/dialogs/default_model_dialog.py
@@ -2,7 +2,7 @@
 
 import tkinter
 
-from gui.dialogs.help_dialog import show_help
+from gui.dialogs.help_dialog import show_help_for_default_model_dialog
 from gui.icons import Icons
 
 
@@ -54,4 +54,4 @@ class DefaultModelSelection(tkinter.Toplevel):
 
     def help(self) -> None:
         """Handle Help button press."""
-        show_help()
+        show_help_for_default_model_dialog()
