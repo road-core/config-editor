46dd614dd959543199342803a75afd765c828334
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-07 09:37:10 +0100

    Help dialog for default provider

--- src/gui/dialogs/help_dialog.py
+++ src/gui/dialogs/help_dialog.py
@@ -123,3 +123,13 @@ def show_help_for_default_model_dialog() -> None:
         ("help text", None),
     ]
     HelpDialog(None, help_text=help_text)
+
+
+def show_help_for_default_provider_dialog() -> None:
+    """Display help for default provider dialog."""
+    help_text: list[tuple[str, Optional[str]]] = [
+        ("<h1>", "Road core service configuration editor"),
+        ("<h2>", "Default provider dialog"),
+        ("help text", None),
+    ]
+    HelpDialog(None, help_text=help_text)
